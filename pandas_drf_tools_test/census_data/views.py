92278273272c583ac2d74699ff3f834b6b8044ac
Agustin Barto <abarto@gmail.com>
2016-09-23 16:03:08 -0300

    Implement viewsets

--- pandas_drf_tools_test/census_data/views.py
+++ pandas_drf_tools_test/census_data/views.py
@@ -6,8 +6,10 @@ from os import path
 from django.core.exceptions import ImproperlyConfigured
 from rest_framework import response, views
 
+from pandas_drf_tools.viewsets import ReadOnlyDataFrameViewSet
 from pandas_drf_tools.serializers import (DataFrameListSerializer, DataFrameIndexSerializer,
                                           DataFrameRecordsSerializer)
+from pandas_drf_tools.pagination import LimitOffsetPagination
 
 
 @lru_cache()
@@ -15,6 +17,8 @@ def get_cc_est2015_alldata_df():
     try:
         data = path.join(path.abspath(path.dirname(__file__)), 'data')
         cc_est2015_alldata_df = pd.read_pickle(path.join(data, 'CC-EST2015-ALLDATA.pkl'))
+        state_df = get_state_df()[['STATE', 'STUSAB']]
+        cc_est2015_alldata_df = cc_est2015_alldata_df.merge(state_df, on=('STATE',))
     except FileNotFoundError as e:
         raise ImproperlyConfigured(
             'Missing data file. Please run the "download_census_data" management command.') from e
@@ -22,12 +26,24 @@ def get_cc_est2015_alldata_df():
     return cc_est2015_alldata_df
 
 
+@lru_cache()
+def get_state_df():
+    try:
+        data = path.join(path.abspath(path.dirname(__file__)), 'data')
+        state_df = pd.read_pickle(path.join(data, 'state.pkl'))
+    except FileNotFoundError as e:
+        raise ImproperlyConfigured(
+            'Missing data file. Please run the "download_census_data" management command.') from e
+
+    return state_df
+
+
 class DataFrameSerializerTestView(views.APIView):
     def get_serializer_class(self):
         raise NotImplementedError()
 
     def get(self, request, *args, **kwargs):
-        sample = get_cc_est2015_alldata_df().sample(10)
+        sample = get_cc_est2015_alldata_df().sample(20)
         serializer = self.get_serializer_class()(sample)
         return response.Response(serializer.data)
 
@@ -56,3 +72,41 @@ class DataFrameRecordsSerializerTestView(DataFrameSerializerTestView):
     def get_serializer_class(self):
         return DataFrameRecordsSerializer
 
+
+class StateEstimatesViewSet(ReadOnlyDataFrameViewSet):
+    serializer_class = DataFrameIndexSerializer
+    pagination_class = LimitOffsetPagination
+
+    def get_dataframe(self):
+        cc_est2015_alldata_df = get_cc_est2015_alldata_df()
+
+        state_names_df = cc_est2015_alldata_df[['STATE', 'STNAME', 'STUSAB']].set_index('STATE').drop_duplicates()
+        population_by_state = cc_est2015_alldata_df\
+            .groupby(['STATE'])\
+            .sum()\
+            .join(state_names_df)\
+            .reset_index()
+
+        return population_by_state
+
+
+class CountyEstimatesViewSet(ReadOnlyDataFrameViewSet):
+    serializer_class = DataFrameIndexSerializer
+    pagination_class = LimitOffsetPagination
+
+    def get_dataframe(self):
+        cc_est2015_alldata_df = get_cc_est2015_alldata_df()
+
+        state_names_df = cc_est2015_alldata_df[['COUNTY', 'CTYNAME', 'STATE']].set_index('COUNTY').drop_duplicates()
+        population_by_county = cc_est2015_alldata_df\
+            .groupby(['COUNTY'])\
+            .sum()\
+            .join(state_names_df)\
+            .reset_index()
+
+        return population_by_county
+
+    def filter_dataframe(self, dataframe):
+        if 'state' in self.request.query_params:
+            dataframe = dataframe[dataframe.STATE == self.request.query_params['state']]
+        return dataframe
--- pandas_drf_tools_test/pandas_drf_tools_test/urls.py
+++ pandas_drf_tools_test/pandas_drf_tools_test/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+from rest_framework.routers import DefaultRouter
+
+
 from census_data.views import (DataFrameListSerializerTestView, DataFrameIndexSerializerTestView,
-                               DataFrameRecordsSerializerTestView)
+                               DataFrameRecordsSerializerTestView, StateEstimatesViewSet, CountyEstimatesViewSet)
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r'states', StateEstimatesViewSet, base_name='state')
+router.register(r'counties', CountyEstimatesViewSet, base_name='county')
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -26,5 +34,6 @@ urlpatterns = [
     url(r'^index_serializer_test/$', DataFrameIndexSerializerTestView.as_view(),
         name='index-serializer-test'),
     url(r'^records_serializer_test/$', DataFrameRecordsSerializerTestView.as_view(),
-        name='records-serializer-test')
+        name='records-serializer-test'),
+    url(r'^api/', include(router.urls)),
 ]
